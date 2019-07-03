acc35bbdb10146aa10853280bdcf5367c4716966
Takeshi Morita <t_morita@keio.jp>
2019-07-03 10:30:38 +0900

    added sota-ros-service-servers-19.7.2.jar
    move and rename setup_printeps.sh

---
+++ install/setup_printeps_sota.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VERSION=19.7.2
+SPEAKERCTL_FILE="speakerctl.sh"
+SOTA_CONFIG_FILE="sota_config.yaml"
+JAR_FILE="sota-ros-service-servers-$VERSION.jar"
+
+if [ ! -e $SPEAKERCTL_FILE ];then
+  wget "https://printeps.org/libs/sota-ros-service-servers/$SPEAKERCTL_FILE"
+fi
+
+if [ ! -e $SOTA_CONFIG_FILE ];then
+  wget "https://printeps.org/libs/sota-ros-service-servers/$SOTA_CONFIG_FILE"
+fi
+
+if [ ! -e $JAR_FILE ];then
+  wget "https://printeps.org/libs/sota-ros-service-servers/$JAR_FILE"
+fi
+env > ~/.ssh/environment
