4787cdf519df0750dd072d22227da3e59424bdeb
Takeshi Morita <t_morita@keio.jp>
2019-07-09 15:29:09 +0900

    added sota-ros-service-servers-19.7.6.jar
    - update setup_printeps_sota.sh

--- install/setup_printeps_sota.sh
+++ install/setup_printeps_sota.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-VERSION=19.7.5
+VERSION=19.7.6
 SPEAKERCTL_FILE="speakerctl.sh"
 SOTA_CONFIG_FILE="sota_config.yaml"
 JAR_FILE="sota-ros-service-servers-$VERSION.jar"
+JETTY_ALPN_AGENT_FILE="jetty-alpn-agent-2.0.9.jar"
 
 if [ ! -e $SPEAKERCTL_FILE ];then
   wget "https://printeps.org/libs/sota-ros-service-servers/$SPEAKERCTL_FILE"
@@ -17,5 +18,8 @@ if [ ! -e $JAR_FILE ];then
   wget "https://printeps.org/libs/sota-ros-service-servers/$JAR_FILE"
 fi
 
+if [ ! -e $JETTY_ALPN_AGENT_FILE ];then
+  wget "https://repo.maven.apache.org/maven2/org/mortbay/jetty/alpn/jetty-alpn-agent/2.0.9/jetty-alpn-agent-2.0.9.jar"
+fi
 
 env > ~/.ssh/environment
